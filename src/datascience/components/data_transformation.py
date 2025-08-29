2b6c27ab801d5a30d8c8d879f03a6bacff22e95a
Pradeep Kumar <pradeepyadav70@live.com>
2025-08-29 08:55:54 +0000

    project changes done

--- src/datascience/components/data_transformation.py
+++ src/datascience/components/data_transformation.py
@@ -8,11 +8,24 @@ class DataTransformation:
     def __init__(self, config: DataTransformationConfig):
         self.config = config
 
+
+
+    def data_transformation(self,titanic):
+        titanic['Age'] = titanic['Age'].fillna(titanic['Age'].mean())
+        titanic['Cabin'] = titanic['Cabin'].fillna('Unknown')
+        titanic['Embarked'] = titanic['Embarked'].fillna('S')
+
+        titanic['Sex'] = titanic['Sex'].map({'male': 0, 'female': 1})
+        titanic['Embarked'] = titanic['Embarked'].map({'S': 0, 'C': 1, 'Q': 2})
+        titanic = titanic.drop(columns=["PassengerId", "Name", "Ticket", "Cabin"], errors="ignore")
+        return titanic
+
     
     ## Note: You can add different data transformation techniques such as Scaler, PCA and all
     
     def train_test_splitting(self):
         data=pd.read_csv(self.config.data_path)
+        data = self.data_transformation(data)
 
         # Split the data into training and test sets. (0.75, 0.25) split.
         train, test = train_test_split(data)
